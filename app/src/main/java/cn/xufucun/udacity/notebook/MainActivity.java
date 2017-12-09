6dd4aa537e60ef271671346285ca8bf8186e7d0c
xufucun <xufucun@gmail.com>
2017-12-09 14:00:26 +0800

    已按照要求修改2

--- app/src/main/java/cn/xufucun/udacity/notebook/MainActivity.java
+++ app/src/main/java/cn/xufucun/udacity/notebook/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_delete_all_entries:
-                deleteDatabase("shelter.db");
+//                deleteDatabase("shelter.db");
                 return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/cn/xufucun/udacity/notebook/data/BillDbHelper.java
+++ app/src/main/java/cn/xufucun/udacity/notebook/data/BillDbHelper.java
@@ -13,8 +13,19 @@ import cn.xufucun.udacity.notebook.data.BillContract.BillEntry;
 public class BillDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "shelter.db";
+    private static final int DATABASE_VERSION = 2;
 
-    private static final int DATABASE_VERSION = 1;
+    // 创建一个包含SQL语句的字符串来创建账单
+    private String SQL_CREATE_BILLS_TABLE = "CREATE TABLE " + BillEntry.TABLE_NAME + " ("
+            + BillEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + BillEntry.COLUMN_BILL_TYPE + " INTEGER NOT NULL DEFAULT 0, "
+            + BillEntry.COLUMN_BILL_PRICE + " INTEGER NOT NULL, "
+            + BillEntry.COLUMN_BILL_REMARK + " TEXT NOT NULL, "
+            + BillEntry.COLUMN_BILL_DATE + " INTEGER NOT NULL);";
+    // 创建一个包含SQL语句的字符串来删除账单
+    private static final String SQL_DELETE_BILLS_TABLE = "DROP TABLE IF EXISTS " + BillEntry.TABLE_NAME;
+
+
 
     public BillDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,24 +33,20 @@ public class BillDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        // 创建一个包含SQL语句的字符串来创建账单
-        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + BillEntry.TABLE_NAME + " ("
-                + BillEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + BillEntry.COLUMN_BILL_TYPE + " INTEGER NOT NULL DEFAULT 0, "
-                + BillEntry.COLUMN_BILL_PRICE + " INTEGER NOT NULL, "
-                + BillEntry.COLUMN_BILL_REMARK + " TEXT NOT NULL, "
-                + BillEntry.COLUMN_BILL_DATE + " INTEGER NOT NULL);";
-
-        // 执行SQL语句
-        sqLiteDatabase.execSQL(SQL_CREATE_PETS_TABLE);
+        //执行sql语句
+        sqLiteDatabase.execSQL(SQL_CREATE_BILLS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        //删除表格并重新创建
+        sqLiteDatabase.execSQL(SQL_DELETE_BILLS_TABLE);
+        onCreate(sqLiteDatabase);
     }
 
-
-
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
 
 }
